769825e8f8ec4ac030b60fab0d592aac25131b4c
Jasa Plohl <jasa.plohl@gmail.com>
2022-01-19 21:22:49 +0100

    Movies API test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,49 @@ import { environment } from 'src/environments/environment';
 })
 export class AppComponent {
   title = 'movie-watch';
+  genres: { "id": Number, "name": String }[];
 
-  apiCallTest() {
-    console.log(environment.API_URL);
+  fetchAvailableGenres() {
+    const urlParams = new URLSearchParams({
+      api_key: environment.API_KEY
+    });
+    const url = environment.API_URL + "/genre/movie/list?" + urlParams;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+        this.genres = response.genres;
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  fetchMoviesByGenre(genreId: Number) {
+    const urlParams = new URLSearchParams({
+      api_key: environment.API_KEY,
+      with_genres: genreId.toString()
+    });
+    const url = environment.API_URL + "/discover/movie?" + urlParams;
+
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  onClickTest(event: MouseEvent) {
+    console.log(event);
   }
 
   constructor() {
-    this.apiCallTest();
+    this.genres = [];
+    this.fetchAvailableGenres();
+    this.fetchMoviesByGenre(27);
   }
 }
