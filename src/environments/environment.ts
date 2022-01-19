b72ffbeb95b3ea1997f74c7b5681728b4eb4fa0e
Jasa Plohl <jasa.plohl@gmail.com>
2022-01-19 20:46:28 +0100

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  API_URL: "https://api.themoviedb.org/3",
+  API_KEY: "9ffe7ff87b4d884b1172d88fd86fd167"
 };
 
 /*
