655e920f6ca9e866b72189c2641d6c6203c72533
Jasa Plohl <jasa.plohl@gmail.com>
2022-01-23 18:14:47 +0100

    Added movie page sections for recommended and similar videos and the cast and reviews.

---
+++ src/app/movie-page/movie-page-recommended/movie-page-recommended.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-page-recommended',
+  templateUrl: './movie-page-recommended.component.html',
+  styleUrls: ['./movie-page-recommended.component.scss']
+})
+export class MoviePageRecommendedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/movie-page/movie-page-similar/movie-page-similar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movie-page-similar',
+  templateUrl: './movie-page-similar.component.html',
+  styleUrls: ['./movie-page-similar.component.scss']
+})
+export class MoviePageSimilarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
