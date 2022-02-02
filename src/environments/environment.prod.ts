3a6a9f574a86fb8be2253e6cf7485bd2b6b52fc4
Jasa Plohl <jasa.plohl@gmail.com>
2022-02-02 23:32:42 +0100

    GitHub pages build.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  API_URL: "https://api.themoviedb.org/3",
+  API_KEY: "9ffe7ff87b4d884b1172d88fd86fd167",
+  IMG_URL: "https://image.tmdb.org/t/p",
+  IMG_SIZE: "w200",
+  IMG_SIZE_LG: "w1280",
+  DEFAULT_AVATAR: "https://ischool.illinois.edu/sites/default/files/styles/normal_square/public/images/empty-avatar-01_1.jpg?itok=hAW4etvU",
+  DEFAULT_IMAGE: "https://images.assetsdelivery.com/compings_v2/yehorlisnyi/yehorlisnyi2104/yehorlisnyi210400016.jpg",
+  PAGE_NUMBERS_DISPLAYED: 5
 };
