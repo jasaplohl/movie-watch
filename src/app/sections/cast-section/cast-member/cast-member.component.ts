b8440bb57a510da42d1503073cfa2584024b6e64
Jasa Plohl <jasa.plohl@gmail.com>
2022-02-02 21:29:30 +0100

    Cast section - clickable cast members.

--- src/app/sections/cast-section/cast-member/cast-member.component.ts
+++ src/app/sections/cast-section/cast-member/cast-member.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ImageService } from '../../../services/image.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cast-member',
@@ -10,10 +11,16 @@ export class CastMemberComponent implements OnInit {
   @Input() declare profile_path: String;
   @Input() declare name: String;
   @Input() declare job: String;
+  @Input() declare person: any;
 
-  constructor(public service: ImageService) { }
+  constructor(public service: ImageService, private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.person);
+  }
+
+  onPersonClick(): void {
+    this.router.navigate(["/person", { id: this.person.id }]);
   }
 
 }
