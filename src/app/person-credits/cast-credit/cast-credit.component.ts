216df6b8b9d80639f083d20d10bb5d4f9b78a43c
Jasa Plohl <jasa.plohl@gmail.com>
2022-02-02 18:11:40 +0100

    Added the credits information on the person page.

--- src/app/person-credits/cast-credit/cast-credit.component.ts
+++ src/app/person-credits/cast-credit/cast-credit.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ImageService } from 'src/app/services/image.service';
 
 @Component({
   selector: 'app-cast-credit',
@@ -8,9 +9,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CastCreditComponent implements OnInit {
   @Input() declare credit: any;
 
-  constructor() { }
+  constructor(public service: ImageService) { }
 
   ngOnInit(): void {
+  }
+
+  getReleaseDate(): any {
+    let date = this.credit.release_date ? this.credit.release_date : this.credit.first_air_date;
+    return date ? date : false;
   }
 
 }
--- src/app/person-credits/crew-credit/crew-credit.component.ts
+++ src/app/person-credits/crew-credit/crew-credit.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ImageService } from 'src/app/services/image.service';
 
 @Component({
   selector: 'app-crew-credit',
@@ -8,9 +9,14 @@ import { Component, Input, OnInit } from '@angular/core';
 export class CrewCreditComponent implements OnInit {
   @Input() declare credit: any;
 
-  constructor() { }
+  constructor(public service: ImageService) { }
 
   ngOnInit(): void {
+  }
+
+  getReleaseDate(): any {
+    let date = this.credit.release_date ? this.credit.release_date : this.credit.first_air_date;
+    return date ? date : false;
   }
 
 }
